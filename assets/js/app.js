3b86fa465bc2492932bc73364895afaafeaa8934
Dipakkkk <dr916139@gmail.com>
2023-06-24 13:56:56 +0530

    section1-complete

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(".section1_slider").slick({
+  slidesToShow: 1,
+  infinite: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  prevArrow: $(".prev"),
+  NextArrow: $(".Next"),
+  autoplaySpeed: 2000,
+//   dots: false, Boolean
+//   arrows: false, Boolean
+});
