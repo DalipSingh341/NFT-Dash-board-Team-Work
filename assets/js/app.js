4b9b27637685f44a96958924f7ed6ad99fe68f97
Dipakkkk <dr916139@gmail.com>
2023-06-26 12:40:14 +0530

    side-bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,23 @@ $(".section1_slider").slick({
 //   dots: false, Boolean
 //   arrows: false, Boolean
 });
+
+// side-bar//
+const sidebar = document.querySelector(".sidebar");
+const navItems = document.querySelectorAll("nav .nav-item");
+const toggle = document.querySelector(".sidebar .toggle");
+
+toggle.addEventListener("click", () => {
+  if (sidebar.className === "sidebar") sidebar.classList.add("open");
+  else sidebar.classList.remove("open");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach((navItem) => {
+      navItem.classList.remove("active");
+    });
+
+    navItem.classList.add("active");
+  });
+});
